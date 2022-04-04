06be53214a6c67ca488d026f759cb959708b691a
Joosua Santasalo <joosua.santasalo@nixu.com>
2022-04-04 14:20:07 +0300

    setup files

--- sh/initForuse.sh
+++ sh/initForuse.sh
@@ -1,3 +1,4 @@
+
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
@@ -6,3 +7,17 @@ export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || pr
 nvm install 14
 
 nvm alias default 14
+
+## setup pandoc for reporting
+
+wget  "https://github.com/jgm/pandoc/releases/download/2.17.1.1/pandoc-2.17.1.1-linux-amd64.tar.gz"; 
+tar xvzf "pandoc-2.17.1.1-linux-amd64.tar.gz" --strip-components 1 -C ~
+
+# Clone the project
+
+git clone https://github.com/jsa2/EAST
+
+cd EAST;
+npm install
+
+echo "CD east to use"
