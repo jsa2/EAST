eea504728ce4a8cb9e7d1f906dccfd207655897b
Joosua Santasalo <joosua.santasalo@nixu.com>
2022-04-05 08:15:17 +0300

    s

--- providers/microsoft.logic/functions/LogicApps_Authorization.js
+++ providers/microsoft.logic/functions/LogicApps_Authorization.js
@@ -7,14 +7,30 @@ module.exports = async function (item) {
 let returnObject = new returnObjectInit(item,__filename.split('/').pop())
 //console.log(stashOrig)
 if (!item?.id.match('microsoft.logic/workflows')) {
-    returnObject.metadata = item?.properties
+    returnObject.metadata = JSON.stringify(item.properties?.accessControl)
     returnObject.isHealthy="notApplicable"
     return returnObject
 }
 
-if (item?.properties?.definition?.triggers?.manual?.kind.toLowerCase() == "http") {
-       console.log('sd')
+returnObject.metadata = JSON.stringify(item.properties?.accessControl)
+returnObject.isHealthy="notApplicable"
+
+if (item?.properties?.definition?.triggers?.manual?.kind.toLowerCase() == "http" && !item.properties?.accessControl?.triggers?.openAuthenticationPolicies) {
+       
+    returnObject.metadata = item?.properties?.definition?.triggers
+    returnObject.isHealthy=false
+   
     }
+
+    
+if (item?.properties?.definition?.triggers?.manual?.kind.toLowerCase() == "http" && item.properties?.accessControl?.triggers?.openAuthenticationPolicies) {
+       
+    returnObject.metadata = item?.properties?.definition?.triggers
+    returnObject.isHealthy=true
+   
+    }
+
+  
 
 
 
