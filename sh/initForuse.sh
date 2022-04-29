ab918f1ccebecb293331a84614418779cbf1d802
Joosua Santasalo <joosua.santasalo@nixu.com>
2022-04-29 17:02:50 +0300

    retry

--- sh/initForuse.sh
+++ sh/initForuse.sh
@@ -20,10 +20,5 @@ git clone https://github.com/jsa2/EAST
 cd EAST;
 npm install
 
-echo "cd EAST to use";
-
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-
-sleep 2;
-exit;
+echo "exit the CloudShell session by typing 'exit' before continuing"
+sleep -s 3
