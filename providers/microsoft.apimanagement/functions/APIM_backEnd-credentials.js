eca84579579e9cd7dcd0521ce710272c51d8d842
Joosua Santasalo <joosua.santasalo@nixu.com>
2022-05-02 18:16:46 +0300

    s

---
+++ providers/microsoft.apimanagement/functions/APIM_backEnd-credentials.js
@@ -0,0 +1,22 @@
+
+const { batchThrottledSimple } = require("../../../plugins/nodeSrc/batch")
+const { AzNodeRest } = require("../../../plugins/nodeSrc/east")
+const { getProviderApiVersion } = require("../../../plugins/nodeSrc/getProvider")
+const { makeSingleArray } = require("../../../plugins/nodeSrc/recursor")
+const { returnObjectInit } = require("../../../plugins/nodeSrc/returnObjectInit")
+
+//AzNodeRest
+module.exports = async function (item) {
+//returnObjectInit
+var returnObject = new returnObjectInit(item,__filename.split('/').pop())
+var {apiversion} = getProviderApiVersion(item.id)
+
+returnObject.isHealthy="review"
+
+let backends = await AzNodeRest(`${item.id}/backends`,apiversion)
+returnObject.metadata={backends:JSON.stringify(backends)}
+
+return returnObject
+
+}
+
