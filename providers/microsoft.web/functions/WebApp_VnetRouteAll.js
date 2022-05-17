e301a1131b71710b1e825167222799d3ac81ca3f
Joosua Santasalo <joosua.santasalo@nixu.com>
2022-05-17 13:44:06 +0300

    providers

---
+++ providers/microsoft.web/functions/WebApp_VnetRouteAll.js
@@ -0,0 +1,38 @@
+const { AzNodeRest } = require("../../../plugins/nodeSrc/east")
+const { returnObjectInit } = require("../../../plugins/nodeSrc/returnObjectInit")
+const { checkDoesItApply } = require("../../../plugins/nodeSrc/microsoftwebhelper")
+//AzNodeRest
+module.exports = async function (item) {
+
+var returnObject = new returnObjectInit(item,__filename.split('/').pop())
+
+var skip = checkDoesItApply(item,returnObject)
+if (skip) {
+    return skip
+}
+
+
+let config = await AzNodeRest(`${item.id}/config/web`,"2020-06-01")
+
+if ( config.properties?.vnetName ) {
+ 
+    if (config.properties?.vnetRouteAllEnabled == false) {
+        returnObject.isHealthy=false
+    }
+
+    if (config.properties?.vnetRouteAllEnabled == true) {
+        returnObject.isHealthy=true
+    }
+
+    
+}
+else {
+    returnObject.isHealthy="not applicable"
+}
+
+returnObject.metadata = config.properties.ipSecurityRestrictions
+
+return returnObject
+
+}
+
