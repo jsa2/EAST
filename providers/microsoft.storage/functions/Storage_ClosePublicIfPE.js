cf8bafd00727021d1605ec6fca57f319194eeece
Joosua Santasalo <joosua.santasalo@nixu.com>
2022-05-17 14:29:37 +0300

    pe updates

--- providers/microsoft.storage/functions/Storage_ClosePublicIfPE.js
+++ providers/microsoft.storage/functions/Storage_ClosePublicIfPE.js
@@ -10,10 +10,10 @@ module.exports = async function (item) {
 let returnObject = new returnObjectInit (item,__filename.split('/').pop())
 
 returnObject.isHealthy="not applicable"
-
+var is 
 if (item.properties?.privateEndpointConnections?.length > 0) {
 
-    let is = (item.properties.networkAcls.ipRules.length > 0 || item.properties.networkAcls.virtualNetworkRules.length  > 0) || undefined
+     is = (item.properties.networkAcls.ipRules.length > 0 || item.properties.networkAcls.virtualNetworkRules.length  > 0) || undefined
 
 if (is == undefined){
     returnObject.isHealthy=false
@@ -26,7 +26,7 @@ if (is == undefined){
 
 
 
-returnObject.metadata = {item}
+returnObject.metadata = {networkAcls: item.properties?.networkAcls, privateEndpointConnections: item.properties?.privateEndpointConnections}
 //console.log(stashOrig)
 
 return returnObject
