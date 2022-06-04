bca9e7dc36147bb241664bf80cf8348b467c8303
jsa <joosua@santasalo.io>
2022-06-04 14:18:48 +0300

    ftps

--- providers/microsoft.web/functions/ftpStatus.js
+++ providers/microsoft.web/functions/ftpStatus.js
@@ -1,18 +1,17 @@
 const { returnObjectInit } = require("../../../plugins/nodeSrc/returnObjectInit")
 const { checkDoesItApply } = require("../../../plugins/nodeSrc/microsoftwebhelper")
+const { AzNodeRest } = require("../../../plugins/nodeSrc/east")
 module.exports = async function (item) {
 
    
     var returnObject = new returnObjectInit(item,__filename.split('/').pop())
 
 
-    var skip = checkDoesItApply(item,returnObject)
-    if (skip) {
-        return skip
-    }
-    var keyToCheck = item?.properties?.siteConfig?.ftpsState
-    returnObject.isHealthy=false
-    if (keyToCheck == null) {
+    let configs = await AzNodeRest(`${item.id}/config/web?`,'2018-11-01')
+  
+    var keyToCheck = configs?.properties?.ftpsState
+
+    if (keyToCheck == "Disabled") {
         returnObject.isHealthy=true 
         keyToCheck="FTP is not allowed"
     }
