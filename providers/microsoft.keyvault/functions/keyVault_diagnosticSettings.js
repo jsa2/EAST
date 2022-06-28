14508f5f68380dbd66143f4e8560fcaabe525850
Joosua Santasalo <joosua.santasalo@nixu.com>
2022-06-28 14:51:12 +0300

    new diagsettings

--- providers/microsoft.keyvault/functions/keyVault_diagnosticSettings.js
+++ providers/microsoft.keyvault/functions/keyVault_diagnosticSettings.js
@@ -1,38 +1,33 @@
 
 
 
-
-
+const { diagReview } = require("../../../plugins/nodeSrc/confirmDiag")
 const { AzNodeRest } = require("../../../plugins/nodeSrc/east")
 const { getProviderApiVersion } = require("../../../plugins/nodeSrc/getProvider")
-const { checkDoesItApply } = require("../../../plugins/nodeSrc/microsoftwebhelper")
 const { returnObjectInit } = require("../../../plugins/nodeSrc/returnObjectInit")
 
 //AzNodeRest
 module.exports = async function (item) {
 
-var returnObject = new returnObjectInit(item,__filename.split('/').pop())
+    var returnObject = new returnObjectInit(item, __filename.split('/').pop())
+    try {
+        //third argument "requireAll" means that all of the categories need to be enabled, default is that single category is required
+        let diag = await diagReview(item, ['audit', 'allLogs'])
+        returnObject.metadata = {
+            diagnostics:JSON.stringify(diag)
+        }
+        returnObject.isHealthy=diag.isHealthy
+        return returnObject
+    } catch (error) {
 
-const diagnostics = await AzNodeRest(`${item.id}/providers/microsoft.insights/diagnosticSettings?`,'2021-05-01-preview')
+        returnObject.metadata = error
+        returnObject.isHealthy="manual"
+        return returnObject
 
-try {
-    var logs =JSON.stringify(diagnostics).match('"categoryGroup\":\"audit\",\"enabled\":true')[0] 
-    returnObject.isHealthy=true
-} catch (error) {
-    console.log()
+    }
+
+
+
 }
-
-
-
-//returnObject.metadata = {logs:{diagnostics:JSON.stringify(diagnostics)}}
-
-returnObject.metadata = {logs:logs || {diagnostics:JSON.stringify(diagnostics)} || "no logs"}
-//console.log(stashOrig)
-
-return returnObject
-
-}
-
-
 
 
